74084612e96b84ddb748b7d6531126e88821e071
Stefan Aberg <stefberg1@gmail.com>
2018-06-11 12:56:09 +0300

    station datafile update to S3. Missing duplicate detection

--- lambda_function.py
+++ lambda_function.py
@@ -4,9 +4,22 @@ import time
 import os
 
 def updateStationsFile(client, list):
-    stationsData = str(time.tm_year) + "," + str(time.tm_yday) + "\n"
+    tm = time.time()
+    timeObj=time.localtime(tm)
+    stationsData = str(timeObj.tm_year) + "," + str(timeObj.tm_yday) + "\n"
+
     for l in list:
         stationsData += l.name + "\n"
+        datafile = 'wind_data/' + l.name + "_" + str(timeObj.tm_year) + "-" + str(timeObj.tm_yday) + ".txt"
+        data = ''
+        try:
+            obj = client.get_object(Bucket='windupdate', Key=datafile)
+            data = obj['Body'].read()
+        except:
+            print "new data", datafile
+        data += str(timeObj.tm_year) + ',' + str(timeObj.tm_mon) + ',' + str(timeObj.tm_mday) + ',' + str(timeObj.tm_hour) + ',' + str(timeObj.tm_min) + ',' + str(l.time) + ',' + str(l.wind_dir) + ',' + str(l.wind_low) + ',' + str(l.wind_speed) + ',' + str(l.wind_max) + ',' + str(l.temp).replace(',','.') + "\n"
+        client.put_object(Body=data, Bucket='windupdate', Key=datafile, ACL='public-read', ContentType='text/plain;charset=utf-8')
+        
     client.put_object(Body=stationsData, Bucket='windupdate', Key='wind_data/stations.txt', ACL='public-read', ContentType='text/plain;charset=utf-8')
 
 def lambda_handler(event, context):
